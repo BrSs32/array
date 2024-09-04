c8491ae5f55177a7dc271530d176cbdf1b4aa452
macaco de botas <85988919231levy@gmail.com>
2024-09-04 20:11:30 -0300

    criei um repositório

---
+++ index.js
@@ -0,0 +1,13 @@
+let pessoas = [
+    'IsmaEl lUstoSA',
+    'nAvARrY lUstoSA',
+    'rEboUÇaS lUstoSA',
+    
+]
+
+//pessoas.map(item =>  console.log(item.toUpperCase()))
+
+//pessoas.forEach(function(item) {
+    //return console.log(item.toUpperCase());
+//})
+
